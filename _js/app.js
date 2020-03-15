a48a0500a538dbd01992e4283a2e2eaa06c54c02
Danilo <danilosantosdeoliveira40@gmail.com>
2020-03-14 23:15:54 -0300

    Animacao do placeholder ao clicar no input

--- _js/app.js
+++ _js/app.js
@@ -7,11 +7,37 @@ function focusFunc(){
 function blurFunc(){
   let parent = this.parentNode.parentNode;
   if (this.value == "") {
-    parent.classList.remove('focus')
+    parent.classList.remove('focus');
   }
 }
+
 
 inputs.forEach(input => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
 })
+
+// function digitouFunc(){
+//   let parent = this.parentNode.parentNode;
+//   parent.classList.add("digitou");
+// }
+// function naodigitouFunc(){
+//   let parent = this.parentNode.parentNode;
+//   if (this.value == "") {
+//     parent.classList.remove('digitou')
+//   }
+// }
+
+// function digitouFunc(){
+//   if (inputs !== "") {
+//     parent.classList.add("digitou");
+//   }else if (inputs == "") {
+//     parent.classList.remove("digitou");
+//   }
+//     // alert("Funcionou");
+// }
+
+// if(document.getElementById("one").value == ""){
+// alert('Por favor, preencha o campo nome');
+// document.getElementById("nome").focus();
+// return false
